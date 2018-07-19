38d68f6a606d37405d9452b8fd93f7606a83e541
munrocket <munrocket@protonmail.com>
2018-07-19 13:09:26 +0400

    test fix

--- src/test.mjs
+++ src/test.mjs
@@ -9,43 +9,43 @@ let test = new ObjectHandler(obj, p => p.arr, p => p.d.y, (p, val) => { p.d.y = 
 let test2 = new ObjectHandler(obj, p => p.arr, p => p.x, (p, val) => { p.x = val });
 
 tape('Square bracket getter', function(t) {
-  t.equal(test[0] == 100, true, 'Accessing by square bracket fail');
+  t.equal(test[0] == 100, true);
   t.end();
 });
 
 tape('Length property', function(t) {
-  t.equal(test.length == obj.arr.length, true, 'Length property not defined');
+  t.equal(test.length == obj.arr.length, true);
   t.end();
 });
 
 tape('Setter accessor', function(t) {
   let temp = test[test.length - 1];
   test[test.length - 1] = "set";
-  t.equal(test[test.length - 1] == "set", true, 'Setter accessor fail');
+  t.equal(test[test.length - 1] == "set", true);
   test[test.length - 1] = temp;
   t.end();
 });
 
-tape('Ordinary property of object', function(t) {
-  t.equal(test['literal'] == 123, true, 'Ordinary property of object unavailable')
+tape('Access to ordinary property', function(t) {
+  t.equal(test['literal'] == 123, true);
   t.end();
 });
 
 tape('Method slice()', function(t) {
-  t.deepEqual(test.slice(0, 2), ['100', '200'], true, 'Method slice() is wrong')
+  t.deepEqual(test.slice(0, 2), ['100', '200']);
   t.end();
 });
 
 tape('Method forEach()', function(t) {
   let temp = []
   test.forEach((item, i) => { temp[i] = item / 100});
-  t.deepEqual(temp, test2.slice(0), true, 'Method forEach() is wrong')
+  t.deepEqual(temp, test2.slice(0));
   t.end();
 });
 
 tape('Method map()', function(t) {
   test2.map((item, i) => item * 100 / (i + 1));
-  t.deepEqual(test2.slice(0), [100,100,100], true, 'Method map() is wrong')
+  t.deepEqual(test2.slice(0), [100,100,100]);
   test2.map((item, i) => i + 1);
   t.end();
 });
@@ -53,35 +53,35 @@ tape('Method map()', function(t) {
 tape('Method filter()', function(t) {
   let p = test.filter((item, i) => parseInt(item) > 150).length / 2;
   let q = test2.filter((item, i) => (item / (i + 1)) == 1).map(x => (x * 100 * p).toString());
-  t.deepEqual(q.slice(0), test.slice(0), true, 'Method filter() is wrong')
+  t.deepEqual(q.slice(0), test.slice(0));
   t.end();
 });
 
 tape('Method reduce()', function(t) {
   let p = test2.reduce((sum, item, i) => sum += item * i, 0);
-  t.equal(p, 8, true, 'Method reduce() is wrong')
+  t.equal(p, 8);
   t.end();
 });
 
 tape('Method every()', function(t) {
   let p = test2.every((item, i) => 1 <= item && item <= 3 && 0 <= i && i < 3);
   let q = test2.every((item, i) => 100 <= item && item <= 300 && 0 <= i && i < 3);
-  t.deepEqual([p, q], [true, false], true, 'Method every() is wrong')
+  t.deepEqual([p, q], [true, false]);
   t.end();
 });
 
 tape('Method reverse()', function(t) {
-  t.deepEqual(test2.reverse().slice(0), [3, 2, 1], true, 'Method every() is wrong')
-  t.end();
+  t.deepEqual(test2.reverse().slice(0), [3, 2, 1]);
   test2.reverse();
+  t.end();
 });
 
 tape('Method join()', function(t) {
-  t.deepEqual(test2.join('*'), "1*2*3", true, 'Method join() is wrong')
+  t.deepEqual(test2.join('*'), "1*2*3");
   t.end();
 });
 
 tape('Method toString()', function(t) {
-  t.deepEqual(test2.toString(), '[ 1, 2, 3 ]', true, 'Method toString() is wrong')
+  t.deepEqual(test2.toString(), '[ 1, 2, 3 ]');
   t.end();
 });
